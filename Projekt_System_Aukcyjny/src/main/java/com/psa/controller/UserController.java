69fe13f067da7f7e4b814abf3886d47fc0c9e184
DESKTOP-CQN9MLJ\figus <figuss03@wp.pl>
2026-06-14 19:03:11 +0200

    Dodanie UserController oraz UserRequestDto

--- Projekt_System_Aukcyjny/src/main/java/com/psa/controller/UserController.java
+++ Projekt_System_Aukcyjny/src/main/java/com/psa/controller/UserController.java
@@ -1,4 +1,46 @@
 package com.psa.controller;
 
+import com.psa.dto.UserRequestDto;
+import com.psa.model.User;
+import com.psa.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserRequestDto requestDto) {
+        return ResponseEntity.ok(userService.createUser(requestDto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody UserRequestDto requestDto) {
+        return ResponseEntity.ok(userService.updateUser(id, requestDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
 }
