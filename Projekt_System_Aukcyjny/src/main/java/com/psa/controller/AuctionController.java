1e4bcdcbd97116628ae04d941a517c7d647e3097
PiotrSz2026 <piotrtab123@gmail.com>
2026-06-17 19:37:40 +0200

    Add files via upload
    Dokończenie kodu

--- Projekt_System_Aukcyjny/src/main/java/com/psa/controller/AuctionController.java
+++ Projekt_System_Aukcyjny/src/main/java/com/psa/controller/AuctionController.java
@@ -19,5 +19,36 @@ public class AuctionController {
         this.auctionService = auctionService;
     }
 
+    @PostMapping
+    public ResponseEntity<Auction> createAuction(@RequestBody AuctionRequestDto auctionRequestDto) {
+        Auction newAuction = auctionService.createAuction(auctionRequestDto);
+        return new ResponseEntity<>(newAuction, HttpStatus.CREATED);
+    }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Auction> updateAuction(@PathVariable Long id, @RequestBody AuctionRequestDto auctionRequestDto) {
+        Auction updatedAuction = auctionService.updateAuction(id, auctionRequestDto);
+        return new ResponseEntity<>(updatedAuction, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Auction> getAuctionById(@PathVariable Long id) {
+        Auction auction = auctionService.getAuctionById(id);
+        return new ResponseEntity<>(auction, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAuction(@PathVariable Long id) {
+        auctionService.deleteAuction(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Auction>> getAllAuctions(
+            @RequestParam(required = false) String category,
+            @RequestParam(required = false) String status
+    ) {
+        List<Auction> auctions = auctionService.getAllAuctions(category, status);
+        return new ResponseEntity<>(auctions, HttpStatus.OK);
+    }
 }
