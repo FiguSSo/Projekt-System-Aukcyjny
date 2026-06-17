7355edbedcdfa6366637fe37f83ec5bc087ccfa9
PiotrSz2026 <piotrtab123@gmail.com>
2026-06-17 19:41:21 +0200

    Dokończenie kodu

--- Projekt_System_Aukcyjny/src/main/java/com/psa/dto/AuctionRequestDto.java
+++ Projekt_System_Aukcyjny/src/main/java/com/psa/dto/AuctionRequestDto.java
@@ -9,4 +9,80 @@ import java.time.LocalDateTime;
 
 public class AuctionRequestDto {
 
+    @NotBlank(message = "Title cannot be blank")
+    private String title;
+
+    @NotBlank(message = "Description cannot be blank")
+    private String description;
+
+    @NotNull(message = "Starting price is required")
+    @Positive(message = "Starting price must be greater than zero")
+    private BigDecimal startingPrice;
+
+    @NotNull(message = "Start date is required")
+    private LocalDateTime startDate;
+
+    @NotNull(message = "End date is required")
+    private LocalDateTime endDate;
+
+    @NotBlank(message = "Category is required")
+    private String category;
+
+    private Long ownerId; // ID właściciela aukcji 
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getStartingPrice() {
+        return startingPrice;
+    }
+
+    public void setStartingPrice(BigDecimal startingPrice) {
+        this.startingPrice = startingPrice;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
 }
